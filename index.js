fcca87e4a04eaf27aaa5fef88397ecf3bf027e8e
Chance Hudson <jchancehud@gmail.com>
2020-12-21 16:18:06 -0600

    piping, request verification

--- index.js
+++ index.js
@@ -21,7 +21,49 @@ const legalMethods = [
   'eth_estimateGas',
   'eth_getTransactionCount',
   'eth_gasPrice',
+  'eth_sendRawTransaction',
+  'eth_sendTransaction'
 ].reduce(objectify, {})
+
+const legalAddresses = [
+  // optimism kovan
+  '0x661e90a3cd113456c1a1f09b1bd3fb5fccf496fd',
+  '0xbc3d14f201194611ece12aa5567e1a950982f6a5',
+  '0xa9c7b1fcbf097d1e58e06d9c4499c4ce42c88e3e',
+].reduce(objectify, {})
+
+function verifyParams(method, params = []) {
+  if (!Array.isArray(params)) {
+    throw new Error('Invalid params argument')
+  }
+  if (method === 'eth_call') {
+    const { to, from } = params[0]
+    if (!legalAddresses[to] && !legalAddresses[from]) {
+      throw new Error(`Neither ${to} or ${from} are whitelisted for ${method}`)
+    }
+  } else if (method === 'eth_getBlockByNumber') {
+    if (params[1] === true) {
+      console.log('getBlockByNumber', params)
+    }
+    // if (params[1] !== false) {
+    //   throw new Error('Unable to retrieve full tx objects')
+    // }
+  } else if (method === 'eth_getLogs') {
+    const { address } = params[0]
+    if (!Array.isArray(address) && !legalAddresses[address]) {
+      throw new Error(`Address ${address} is not whitelisted for ${method}`)
+    }
+    for (const addr of address) {
+      if (legalAddresses[addr]) continue
+      throw new Error(`Address ${addr} is not whitelisted for ${method}`)
+    }
+  } else if (method === 'eth_estimateGas') {
+    const { from, to } = params[0]
+    if (!legalAddresses[from] && !legalAddresses[to]) {
+      throw new Error(`Neither ${from} or ${to} are whitelisted for ${method}`)
+    }
+  }
+}
 
 app.post('/', async (req, res) => {
   const { jsonrpc, method, params, id } = req.body
@@ -31,17 +73,26 @@ app.post('/', async (req, res) => {
     res.status(400).json({ message: `jsonrpc version 2.0 required` })
     return
   }
-  // if (!legalMethods[method]) {
-  //   res.status(405).json({ message: `Method ${method} is not allowed` })
-  //   return
-  // }
+  if (!legalMethods[method]) {
+    res.status(405).json({ message: `Method ${method} is not allowed` })
+    return
+  }
   log(method, params)
-
+  try {
+    verifyParams(method, params)
+  } catch (err) {
+    res.status(401).json({ message: err.toString() })
+    return
+  }
   const gethUrl = 'https://kovan.infura.io/v3/6d3a403359fb4784b12a4cf6ed9f8ddd'
 
-  const { data, ...o } = await axios.post(gethUrl, req.body)
-  console.log(data, o)
-  res.json(data)
+  const { data } = await axios({
+    method: 'post',
+    url: gethUrl,
+    responseType: 'stream',
+    data: req.body,
+  })
+  data.pipe(res)
 })
 
 const logPath = path.join(__dirname, 'requests.log')
